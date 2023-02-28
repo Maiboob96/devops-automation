48e3868dee26b2101df7241ff709c09592ba003a
Mehboob <maiboobch@gmail.com>
2023-02-28 16:48:30 +0530

    'first'

---
+++ src/com/JavaPrgms/Inheritance.java
@@ -0,0 +1,21 @@
+package com.JavaPrgms;
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Java prgms flrfk;reffmekrrlmwe");
+        // Variables
+        String name = "Mehaboob";
+        int age = 24;
+        String neihbour = "Anjali";
+
+        String friend = "neihbour";
+         System.out.println(name);
+         System.out.println(neihbour);
+
+
+
+
+    }
+}
---
+++ src/com/JavaPrgms/Main.java
@@ -0,0 +1,16 @@
+package com.JavaPrgms;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Non-Primitive Dta Types
+
+       String name = "Mehaboob";
+       String name2 = name.replace('h','s');
+
+        System.out.println(name2);
+        System.out.println(name);
+
+
+    }
+}
